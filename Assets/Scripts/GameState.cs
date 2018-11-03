74b4866f3e7b6da4177063ba8295c3908b011516
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-11-03 12:27:16 -0700

    basic scoring mechanics

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 //Should only have one of these classes initalized at once
 public class GameState {
-  public static int totalScore = 4;
-  public static int timeForLevel = 3;
+
+  private static int totalScore = 4;
+  private static int timeForLevel = 3;
+
+  private static GameStatePlot[] plots;
+  private static int plotCurrentIndex = 0;
+  private static int plotTotalCount = 4;
+
+  public static void GeneratePlots () {
+    plots = new GameStatePlot[plotTotalCount];
+  }
 
   public static int TotalScore {
     get {
-      return totalScore;
-    }
-    set {
-      totalScore = value;
+      int _totalScore = 0;
+      for (int i = 0; i < plotTotalCount; i++) {
+        _totalScore += plots[plotCurrentIndex].Score;
+      }
+      return _totalScore;
     }
   }
 
@@ -23,4 +33,20 @@ public class GameState {
     }
   }
 
+  public static GameStatePlot PlotCurrent {
+    get {
+      return plots[plotCurrentIndex];
+    }
+  }
+
+  public static int PlotCurrentIndex {
+    set {
+      plotCurrentIndex = value;
+    }
+  }
+
+}
+
+public class GameStatePlot {
+  public int Score { get; set; }
 }
