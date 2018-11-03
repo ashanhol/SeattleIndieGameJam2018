6aaf66afbde288dc3927277636cdd461de03e2c7
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-11-03 13:03:01 -0700

    re-add planting mechanics

---
+++ Assets/Scripts/PlantMechanics.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+//Should only have one of these classes initalized at once
+public class PlantingMechanics {
+  private static int numTotalPlots_ = 4;
+
+  //Data structure for keeping track of farm plots
+  public static PlantObject[] plots = new PlantObject[numTotalPlots_];
+
+  //TODO: Need function for matching score with plant
+
+}
+
+public class PlantObject {
+  //Score
+  int Score { get; set; }
+
+  //TODO: Function for keeping track of Actions taken on which turn
+
+}
