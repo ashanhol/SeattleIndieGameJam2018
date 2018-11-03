d9b789e93f7fa5c7f5effcb5b85620b41554da1d
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-11-03 13:49:30 -0700

    add state and action logic

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -3,67 +3,13 @@ using UnityEngine;
 //Should only have one of these classes initalized at once
 public class GameState {
 
-  private static int totalScore = 4;
-  private static int timeForLevel = 3;
-  private static int currentLevelLoop = 0;
+  public static PlantObject[] Plots { get; set; }
+  public static int TotalPlotCount { get; set; }
+  public static int TotalPlotCountLastRound { get; set; }
+  public static int CurrentPlotIndex { get; set; }
+  public static int LapsRunThroughLoop { get; set; }
 
-  private static GameStatePlot[] plots;
-  private static int currentPlotIndex = 0;
-  private static int totalPlotCount = 4;
-  private static int totalPlotCountLastRound = 0;
+  // TODO: use this!
+  public static int TimeForLevel { get; set; }
 
-  public static void GenerateLevel () {
-    plots = new GameStatePlot[totalPlotCount];
-    totalPlotCountLastRound = totalPlotCount;
-    totalPlotCount = totalPlotCount + 4;
-  }
-
-  public static void AdvanceLoop () {
-    currentPlotIndex = 0;
-    ++currentLevelLoop;
-  }
-
-  public static void doAction () { }
-
-  public static GameStatePlot[] Plots {
-    get {
-      return plots;
-    }
-  }
-
-  public static int TotalScore {
-    get {
-      int _totalScore = 0;
-      for (int i = 0; i < totalPlotCount; i++) {
-        _totalScore += plots[currentPlotIndex].Score;
-      }
-      return _totalScore;
-    }
-  }
-
-  public static int TimeForLevel {
-    get {
-      return timeForLevel;
-    }
-    set {
-      timeForLevel = value;
-    }
-  }
-
-  public static GameStatePlot CurrentPlot {
-    get {
-      return plots[currentPlotIndex];
-    }
-  }
-
-  public static int CurrentPlotIndex {
-    set {
-      currentPlotIndex = value;
-    }
-  }
-
-}
-
-public class GameStatePlot {
-  public int Score { get; set; }
 }
--- Assets/Scripts/PlantMechanics.cs
+++ Assets/Scripts/PlantMechanics.cs
@@ -1,6 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-enum PlayerAction {
+public enum PlayerAction : int {
   Seed = 20,
   Water = 10,
   Fertilize = 5,
@@ -8,19 +10,46 @@ enum PlayerAction {
 
 //Should only have one of these classes initalized at once
 public class PlantingMechanics {
-  private static int numTotalPlots_ = 4;
 
-  //Data structure for keeping track of farm plots
-  public static PlantObject[] plots = new PlantObject[numTotalPlots_];
+  public static void GenerateLevel () {
+    GameState.Plots = new PlantObject[GameState.TotalPlotCount];
+    GameState.TotalPlotCountLastRound = GameState.TotalPlotCount;
+    GameState.TotalPlotCount = GameState.TotalPlotCount + 4;
+    GameState.LapsRunThroughLoop = 0;
+  }
 
-  //TODO: Need function for matching score with plant
+  public static void AdvanceLoop () {
+    GameState.CurrentPlotIndex = 0;
+    ++GameState.LapsRunThroughLoop;
+  }
+
+  //Need function for matching score with plant
+  public static void doPlayerAction (int PlayerActionValue) {
+    CurrentPlot.Actions.Add(PlayerActionValue);
+    CurrentPlot.Score += PlayerActionValue * GameState.LapsRunThroughLoop;
+  }
+
+  public static int TotalScore {
+    get {
+      int _totalScore = 0;
+      for (int i = 0; i < GameState.TotalPlotCount; i++) {
+        _totalScore += CurrentPlot.Score;
+      }
+      return _totalScore;
+    }
+  }
+
+  public static PlantObject CurrentPlot {
+    get {
+      return GameState.Plots[GameState.CurrentPlotIndex];
+    }
+  }
 
 }
 
 public class PlantObject {
   //Score
-  int Score { get; set; }
-
-  //TODO: Function for keeping track of Actions taken on which turn
-
+  public int Score { get; set; }
+  //For keeping track of Actions taken on which turn
+  public List<int> Actions { get; set; }
 }
