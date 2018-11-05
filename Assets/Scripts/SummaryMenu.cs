b6e41e0a70f3c864203683c9e4e222fde90ae583
Rachél Bazelais <bazelais.rachel@gmail.com>
2018-11-04 17:23:48 -0800

    Summary menu text

---
+++ Assets/Scripts/SummaryMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SummaryMenu : MonoBehaviour
+{
+    public GameObject summaryMenuUI;
+
+    public void ReplayGame()
+    {
+        //Load the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void GoToMainMenu()
+    {
+        //Load the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
